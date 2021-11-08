daa8d91d079b739d0e5a06a56d6f1621fc22d9c9
true_mogician <925269713@qq.com>
2021-11-08 14:54:23 +0800

    fix: token patterns

--- CMinusMinus/Lexer.cs
+++ CMinusMinus/Lexer.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Text.RegularExpressions;
 using Lexer;
-using Microsoft.Extensions.Primitives;
 
 namespace CMinusMinus {
 	public partial class CMinusMinus {
-		public static readonly string[] Keywords = {"int", "void", "if", "else", "while", "return"};
+		public static readonly string[] Keywords = {"char", "short", "int", "long", "float", "double", "void", "if", "else", "do", "while", "return"};
 
 		public Lexer<TokenType> Lexer { get; } = new();
 
@@ -18,20 +16,20 @@ namespace CMinusMinus {
 				TokenType.Identifier,
 				code => keywordToken.Match(code) is null ? identifierPattern.Match(code) : null
 			);
-			Lexer.AddRule(TokenType.IntegerLiteral, @"[-+]?(?:\d+|0b[01]+|0x[\da-fA-F])");
-			Lexer.AddRule(TokenType.FloatLiteral, @"[-+]?\d+(?:(?:\.\d+)?e[-+]?\d+|\.\d+)");
+			Lexer.AddRule(TokenType.FloatLiteral, @"[-+]?\d+(?:(?:\.\d+)?[eE][-+]?\d+|\.\d+)");
+			Lexer.AddRule(TokenType.IntegerLiteral, @"[-+]?(?:\d+(?![-+\.eE])|0b[01]+|0x[\da-fA-F])");
 			const string escapedCharPattern = @"\\(?:[abefnrtv\\'""]|[0-7]{1,3}|x[\da-fA-F]{1,2}|u[\da-fA-F]{4})";
-			Lexer.AddRule(TokenType.CharacterLiteral, $@"'[^\\']|{escapedCharPattern}'", 6);
-			Lexer.AddRule(TokenType.StringLiteral, $@"L?""(?:[^\\'""]|{escapedCharPattern})*""");
+			Lexer.AddRule(TokenType.CharacterLiteral, $@"'(?:[^\\']|{escapedCharPattern})'", 8);
+			Lexer.AddRule(TokenType.StringLiteral, $@"L?""(?:[^\\""]|{escapedCharPattern})*""");
 			Lexer.AddRule(TokenType.AssignmentOperator, @"=", 1);
 			Lexer.AddRule(TokenType.ArithmeticOperator, @"[-+*/%^&|]|<<|>>>?", 3);
 			Lexer.AddRule(TokenType.ComparisonOperator, @"[<>]|[<>=!]=", 2);
 			Lexer.AddRule(TokenType.LogicalOperator, @"&&|\|\|", 2);
-			Lexer.AddRule(TokenType.BoundaryMark, @";", 1);
+			Lexer.AddRule(TokenType.SentenceTerminator, @";", 1);
 			Lexer.AddRule(TokenType.Separator, @",", 1);
-			Lexer.AddRule(TokenType.CommentMark, @"\/\/|\/\*|\*\/", 2);
+			Lexer.AddRule(TokenType.CommentBoundary, @"\/\/|\/\*|\*\/", 2);
 			Lexer.AddRule(TokenType.Parenthesis, @"[()]", 1);
-			Lexer.AddRule(TokenType.Bracket, @"[{}]", 1);
+			Lexer.AddRule(TokenType.BlockBoundary, @"[{}]", 1);
 			Lexer.AddRule(TokenType.WhiteSpace, @"\s+");
 		}
 	}
@@ -57,15 +55,15 @@ namespace CMinusMinus {
 
 		LogicalOperator,
 
-		BoundaryMark,
+		SentenceTerminator,
 
 		Separator,
 
-		CommentMark,
+		CommentBoundary,
 
 		Parenthesis,
 
-		Bracket,
+		BlockBoundary,
 
 		WhiteSpace
 	}
