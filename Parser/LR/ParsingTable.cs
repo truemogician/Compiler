fe1795330e6859ece94f7621d65694345ad55699
true_mogician <925269713@qq.com>
2021-11-14 00:29:07 +0800

    feat: ExtendGrammar, constructor workflow

--- Parser/LR/ParsingTable.cs
+++ Parser/LR/ParsingTable.cs
@@ -1,13 +1,28 @@
-﻿using System;
+﻿using System.Linq;
 using TrueMogician.Exceptions;
 
+// ReSharper disable VirtualMemberCallInConstructor
 namespace Parser.LR {
 	public abstract class ParsingTable<TItem> where TItem : ItemBase {
-		protected internal ParsingTable() { }
+		protected internal ParsingTable(Grammar grammar) {
+			Grammar = grammar;
+			ExtendedGrammar = ExtendGrammar(grammar);
+			FirstSetCollection = new FirstSetCollection(ExtendedGrammar);
+			if (!BuildTables(out var actionTable, out var gotoTable))
+				throw new ParserException();
+			ActionTable = actionTable;
+			GotoTable = gotoTable;
+		}
 
-		public ActionTable<TItem> ActionTable { get; } = new();
+		public Grammar Grammar { get; }
 
-		public GotoTable<TItem> GotoTable { get; } = new();
+		public ActionTable<TItem> ActionTable { get; }
+
+		public GotoTable<TItem> GotoTable { get; }
+
+		protected Grammar ExtendedGrammar { get; }
+
+		protected FirstSetCollection FirstSetCollection { get; }
 
 		public IAction this[ItemSetBase<TItem> state, Symbol symbol] {
 			get => symbol.IsTerminal
@@ -27,6 +42,17 @@ namespace Parser.LR {
 			}
 		}
 
-		public static Grammar ExtendGrammar() => throw new NotImplementedException();
+		protected static Grammar ExtendGrammar(Grammar grammar) {
+			var newGrammar = new Grammar(grammar);
+			var initial = new Nonterminal("_Start");
+			var nonterminals = newGrammar.Nonterminals.ToList();
+			while (nonterminals.Contains(initial))
+				initial = new Nonterminal("_" + initial.Name);
+			newGrammar.AddProductionRule(initial, newGrammar.InitialState!);
+			newGrammar.InitialState = initial;
+			return newGrammar;
+		}
+
+		protected abstract bool BuildTables(out ActionTable<TItem> actionTable, out GotoTable<TItem> gotoTable);
 	}
 }
