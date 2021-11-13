229ee74524e96ba09ddd6a78eef677c767ba5757
true_mogician <925269713@qq.com>
2021-11-14 00:28:20 +0800

    feat: memberwise equality, range index, + operator

--- Parser/SentenceForm.cs
+++ Parser/SentenceForm.cs
@@ -4,10 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace Parser {
-	public class SentenceForm : IList<Symbol> {
+	public class SentenceForm : IList<Symbol>, IEquatable<SentenceForm> {
 		private readonly List<Symbol> _list = new();
 
 		private SentenceForm() { }
+
+		public SentenceForm(IEnumerable<Symbol> items) : this(items.ToArray()) { }
 
 		public SentenceForm(params Symbol[] items) => _list.AddRange(items);
 
@@ -24,6 +26,24 @@ namespace Parser {
 		public Symbol this[int index] {
 			get => _list[index];
 			set => _list[index] = value;
+		}
+
+		public Symbol this[Index index] {
+			get => _list[index];
+			set => _list[index] = value;
+		}
+
+		public SentenceForm this[Range range] {
+			get {
+				var arr = new Symbol[range.End.Value - range.Start.Value];
+				for (var i = 0; i < arr.Length; ++i)
+					arr[i] = _list[i + range.Start.Value];
+				return new SentenceForm(arr);
+			}
+			set {
+				for (var i = 0; i < value.Count; ++i)
+					_list[i + range.Start.Value] = value[i];
+			}
 		}
 
 		public IEnumerator<Symbol> GetEnumerator() => _list.GetEnumerator();
@@ -46,8 +66,32 @@ namespace Parser {
 
 		public void RemoveAt(int index) => _list.RemoveAt(index);
 
+		public bool Equals(SentenceForm other) {
+			if (other is null)
+				return false;
+			if (ReferenceEquals(this, other))
+				return true;
+			if (_list.Count != other.Count)
+				return false;
+			return !_list.Where((t, i) => !t.Equals(other._list[i])).Any();
+		}
+
+		public override bool Equals(object obj) {
+			if (obj is null)
+				return false;
+			if (ReferenceEquals(this, obj))
+				return true;
+			return obj.GetType() == GetType() && Equals((SentenceForm)obj);
+		}
+
+		public override int GetHashCode() => _list != null ? _list.GetHashCode() : 0;
+
+		public static implicit operator SentenceForm(Symbol symbol) => new(symbol);
+
 		public static implicit operator SentenceForm(Nonterminal nonTerminal) => new(nonTerminal);
 
 		public static implicit operator SentenceForm(Terminal terminal) => new(terminal);
+
+		public static SentenceForm operator +(SentenceForm left, SentenceForm right) => new(left._list.Concat(right._list).ToArray());
 	}
 }
