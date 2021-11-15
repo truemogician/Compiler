dd542e564430c4b9c6919a33cc736bd6617b38dd
true_mogician <925269713@qq.com>
2021-11-16 02:42:53 +0800

    refactor: class -> record

--- Parser/Nonterminal.cs
+++ Parser/Nonterminal.cs
@@ -2,38 +2,13 @@
 
 #nullable enable
 namespace Parser {
-	public class Nonterminal : IEquatable<Nonterminal> {
+	public record Nonterminal(string Name, bool Temporary = false) {
 		/// <summary>
 		/// Create a new temporary nonterminal, whose name will be a GUID
 		/// </summary>
 		public Nonterminal() : this(Guid.NewGuid().ToString("N"), true) { }
 
-		public Nonterminal(string name, bool temporary = false) {
-			Name = name;
-			Temporary = temporary;
-		}
-
-		public string Name { get; }
-
-		public bool Temporary { get; }
-
 		public override string ToString() => Name;
-
-		public bool Equals(Nonterminal? other) {
-			if (other is null)
-				return false;
-			return ReferenceEquals(this, other) || Name == other.Name && Temporary == other.Temporary;
-		}
-
-		public override bool Equals(object? obj) {
-			if (obj is null)
-				return false;
-			if (ReferenceEquals(this, obj))
-				return true;
-			return obj.GetType() == GetType() && Equals(obj as Nonterminal);
-		}
-
-		public override int GetHashCode() => HashCode.Combine(Name, Temporary);
 
 		public static implicit operator Nonterminal(string name) => new(name);
 
