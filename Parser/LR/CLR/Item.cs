ee4068a62f493eed69dd4bafd43979053d8dd181
true_mogician <925269713@qq.com>
2021-11-16 12:19:58 +0800

    feat: ToString

--- Parser/LR/CLR/Item.cs
+++ Parser/LR/CLR/Item.cs
@@ -1,3 +1,5 @@
 ﻿namespace Parser.LR.CLR {
-	public record Item(ProductionRule ProductionRule, int Marker, Terminal Lookahead) : ItemBase(ProductionRule, Marker);
+	public record Item(ProductionRule ProductionRule, int Marker, Terminal Lookahead) : ItemBase(ProductionRule, Marker) {
+		public override string ToString() => $"{base.ToString()}, {Lookahead}";
+	}
 }
--- Parser/LR/ItemBase.cs
+++ Parser/LR/ItemBase.cs
@@ -1,5 +1,7 @@
 ﻿namespace Parser.LR {
 	public abstract record ItemBase(ProductionRule ProductionRule, int Marker) {
 		public Symbol NextSymbol => Marker == ProductionRule.Length ? Terminal.Terminator : ProductionRule[Marker];
+
+		public override string ToString() => $"{ProductionRule.Nonterminal} -> {ProductionRule.Production[..Marker]}●{ProductionRule.Production[Marker..]}";
 	}
 }
