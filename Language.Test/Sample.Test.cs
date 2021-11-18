d4ab57afcfe078864307476c66befd6e6d3a7be2
true_mogician <925269713@qq.com>
2021-11-18 15:35:47 +0800

    test: sample language test

---
+++ Language.Test/Sample.Test.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Lexer;
+using NUnit.Framework;
+using Parser;
+using Parser.LR;
+
+#nullable enable
+namespace Language.Test {
+	public class SampleTests {
+		public Sample Language { get; } = new();
+
+		[Test]
+		public void ParsingTableTest() {
+			var table = Language.Parser.ParsingTable;
+			Assert.AreEqual(table.ItemSets.InitialState.Count, 6);
+			Assert.AreEqual(table.ItemSets.Count, 10);
+			Assert.AreEqual(table.ActionTable[table.GotoTable[table.ItemSets.InitialState, "S"]!, Terminal.Terminator]!.Type, ActionType.Accept);
+		}
+
+		[SuppressMessage("ReSharper", "StringLiteralTypo")]
+		[TestCase("abab", ExpectedResult = null)]
+		[TestCase("aba", ExpectedResult = typeof(NotRecognizedException))]
+		[TestCase("ac", ExpectedResult = typeof(TokenNotMatchedException))]
+		public Type? ParseTest(string code) {
+			try {
+				Console.Write(Language.Parse(code).ToString());
+				return null;
+			}
+			catch (ParserException ex) {
+				return ex.GetType();
+			}
+			catch (LexerException ex) {
+				return ex.GetType();
+			}
+		}
+	}
+}
---
+++ Language.Test/Sample.cs
@@ -0,0 +1,19 @@
+﻿using Lexer;
+using Parser;
+
+namespace Language.Test {
+	public class Sample : Language<Lexer.Lexer, Parser.LR.CLR.Parser> {
+		public Sample() {
+			var lexicon = new Lexicon {{"a", 'a'}, {"b", 'b'}};
+			Lexer = new Lexer.Lexer(lexicon);
+			var s = new Nonterminal("S");
+			var b = new Nonterminal("B");
+			var grammar = new Grammar(s) {{s, b + b}, {b, lexicon["b"]}, {b, (SentenceForm)lexicon["a"] + b}};
+			Parser = new Parser.LR.CLR.Parser(grammar);
+		}
+
+		public override Lexer.Lexer Lexer { get; }
+
+		public override Parser.LR.CLR.Parser Parser { get; }
+	}
+}
