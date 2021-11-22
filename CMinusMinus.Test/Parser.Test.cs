54287972c4c72c52dc8e347500d2a6abfb32e483
true_mogician <925269713@qq.com>
2021-11-22 14:04:09 +0800

    test: check exception type

--- CMinusMinus.Test/Parser.Test.cs
+++ CMinusMinus.Test/Parser.Test.cs
@@ -1,27 +1,31 @@
+using System;
 using System.IO;
 using NUnit.Framework;
+using Parser;
 
+#nullable enable
 namespace CMinusMinus.Test {
 	public class ParserTests {
 		public static readonly CMinusMinus Language = new();
 
-		[TestCase(@"int func(){}", ExpectedResult = true)]
-		[TestCase(@"'\'", ExpectedResult = false)]
-		public bool LiteralTest(string code) {
-			var lexemes = Language.Lexer.Tokenize(code);
+		[TestCase(@"long long int", ExpectedResult = null)]
+		[TestCase(@"unsigned long long", ExpectedResult = null)]
+		[TestCase(@"long double", ExpectedResult = null)]
+		[TestCase(@"signed", ExpectedResult = null)]
+		public Type? LiteralTest(string code) {
 			try {
-				var tree = Language.Parser.Parse(lexemes);
-				return true;
+				Console.WriteLine(Language.Parse(code).ToString());
+				return null;
 			}
-			catch {
-				return false;
+			catch (ParserException ex) {
+				return ex.GetType();
 			}
 		}
 
-		[TestCase(@"samples/0.cmm", ExpectedResult = true)]
-		[TestCase(@"samples/a+b.cmm", ExpectedResult = true)]
-		[TestCase(@"samples/hello.cmm", ExpectedResult = true)]
-		[TestCase(@"samples/literal.cmm", ExpectedResult = true)]
-		public bool FileTest(string filePath) => LiteralTest(File.ReadAllText(filePath));
+		[TestCase(@"samples/0.cmm", ExpectedResult = null)]
+		[TestCase(@"samples/a+b.cmm", ExpectedResult = null)]
+		[TestCase(@"samples/hello.cmm", ExpectedResult = null)]
+		[TestCase(@"samples/literal.cmm", ExpectedResult = null)]
+		public Type? FileTest(string filePath) => LiteralTest(File.ReadAllText(filePath));
 	}
 }
