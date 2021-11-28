18d1ae038c2ab6f7c430ce21cf50d447384243c4
true_mogician <925269713@qq.com>
2021-11-28 10:36:47 +0800

    perf: sync calculation

--- Parser/LR/CLR/ParsingTable.cs
+++ Parser/LR/CLR/ParsingTable.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq;
-using System.Threading.Tasks;
 
 namespace Parser.LR.CLR {
 	using ActionFactory = ActionFactory<Item>;
@@ -16,31 +15,21 @@ namespace Parser.LR.CLR {
 			GotoTable = new GotoTable();
 			var acceptItem = new Item(extendedGrammar[extendedGrammar.InitialState].Single(), 1, Terminal.Terminator);
 			OnStartTableCalculation();
-			Task.WaitAll(
-				ItemSets.Select(
-						state => Task.Run(
-							() => {
-								lock (ActionTable) {
-									foreach (var item in state)
-										if (item == acceptItem)
-											ActionTable[state, Terminal.Terminator] = ActionFactory.AcceptAction;
-										else if (item.NextSymbol is null)
-											ActionTable[state, item.Lookahead] = ActionFactory.CreateReduceAction(item.ProductionRule);
-										else if (item.NextSymbol.IsTerminal) {
-											var nextTerminal = item.NextSymbol.AsTerminal;
-											ActionTable[state, nextTerminal] = ActionFactory.CreateShiftAction(ItemSets.Go(state, nextTerminal));
-										}
-										else {
-											var nextSymbol = item.NextSymbol;
-											lock (GotoTable)
-												GotoTable[state, nextSymbol.AsNonterminal] = ItemSets.Go(state, nextSymbol);
-										}
-								}
-							}
-						)
-					)
-					.ToArray()
-			);
+			foreach (var state in ItemSets) {
+				foreach (var item in state)
+					if (item == acceptItem)
+						ActionTable[state, Terminal.Terminator] = ActionFactory.AcceptAction;
+					else if (item.NextSymbol is null)
+						ActionTable[state, item.Lookahead] = ActionFactory.CreateReduceAction(item.ProductionRule);
+					else if (item.NextSymbol.IsTerminal) {
+						var nextTerminal = item.NextSymbol.AsTerminal;
+						ActionTable[state, nextTerminal] = ActionFactory.CreateShiftAction(ItemSets.Go(state, nextTerminal));
+					}
+					else {
+						var nextSymbol = item.NextSymbol;
+						GotoTable[state, nextSymbol.AsNonterminal] = ItemSets.Go(state, nextSymbol);
+					}
+			}
 			OnCompleteTableCalculation();
 		}
 	}
