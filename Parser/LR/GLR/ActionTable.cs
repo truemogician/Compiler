63a8f5c8870a81f001a4d5363752b62f2e10ad6a
true_mogician <925269713@qq.com>
2021-12-06 01:50:45 +0800

    feat: GLR ParsingTable

---
+++ Parser/LR/GLR/ActionTable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Parser.LR.GLR {
+	using Item = CLR.Item;
+	using ItemSet = ItemSet<CLR.Item>;
+
+	public class ActionTable : ActionTable<Item, List<IAction>> {
+		/// <returns>If not found, an empty <see cref="List{IAction}" /> will be returned</returns>
+		public override List<IAction> this[ItemSet state, Terminal terminal] {
+			get => Table.ContainsKey(state) && Table[state].ContainsKey(terminal) ? Table[state][terminal] : new List<IAction>();
+			set => base[state, terminal] = value;
+		}
+	}
+}
---
+++ Parser/LR/GLR/ParsingTable.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Parser.LR.CLR;
+
+namespace Parser.LR.GLR {
+	using ActionFactory = ActionFactory<Item>;
+
+	public class ParsingTable : ParsingTableBase<Item, List<IAction>> {
+		internal ParsingTable(Grammar grammar) : base(grammar) { }
+
+		/// <param name="checkConflicts">
+		///     GLR parser preserve all conflicts, thus <paramref name="checkConflicts" /> will be ignored
+		///     in this implementation
+		/// </param>
+		protected override void Initialize(Grammar extendedGrammar, bool checkConflicts = true) {
+			ItemSets = new ItemSetCollection(extendedGrammar);
+			OnStartItemSetsCalculation();
+			ItemSets.Initialize();
+			OnCompleteItemSetsCalculation();
+			ActionTable = new ActionTable();
+			GotoTable = new GotoTable();
+			var acceptItem = new Item(extendedGrammar[extendedGrammar.InitialState].Single(), 1, Terminal.Terminator);
+			OnStartTableCalculation();
+			foreach (var state in ItemSets) {
+				foreach (var item in state)
+					if (item == acceptItem)
+						ActionTable[state, Terminal.Terminator] = new List<IAction> {ActionFactory.AcceptAction};
+					else if (item.NextSymbol is null) {
+						var reduceAction = ActionFactory.CreateReduceAction(item.ProductionRule);
+						var actions = ActionTable[state, item.Lookahead];
+						if (actions.Count == 0) {
+							actions.Add(reduceAction);
+							ActionTable[state, item.Lookahead] = actions;
+						}
+						else if (actions.IndexOf(reduceAction) == -1)
+							actions.Add(reduceAction);
+					}
+					else if (item.NextSymbol.IsTerminal) {
+						var nextTerminal = item.NextSymbol.AsTerminal;
+						var shiftAction = ActionFactory.CreateShiftAction(ItemSets.Go(state, nextTerminal));
+						var actions = ActionTable[state, nextTerminal];
+						if (actions.Count == 0) {
+							actions.Add(shiftAction);
+							ActionTable[state, nextTerminal] = actions;
+						}
+						else if (actions.IndexOf(shiftAction) == -1)
+							actions.Add(shiftAction);
+					}
+					else {
+						var nextSymbol = item.NextSymbol;
+						GotoTable[state, nextSymbol.AsNonterminal] = ItemSets.Go(state, nextSymbol);
+					}
+			}
+			OnCompleteTableCalculation();
+		}
+	}
+}
