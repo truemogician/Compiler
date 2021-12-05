4857041cebc9ab851eb2738804f894ce940cceda
true_mogician <925269713@qq.com>
2021-12-06 01:49:48 +0800

    feat: checkConflicts in Initialize

--- Parser/LR/CLR/ParsingTable.cs
+++ Parser/LR/CLR/ParsingTable.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace Parser.LR.CLR {
 	using ActionFactory = ActionFactory<Item>;
 
-	public class ParsingTable : ParsingTableBase<Item> {
+	public class ParsingTable : ParsingTableBase<Item, IAction> {
 		public ParsingTable(Grammar grammar) : base(grammar) { }
 
-		protected override void Initialize(Grammar extendedGrammar) {
+		protected override void Initialize(Grammar extendedGrammar, bool checkConflicts = true) {
 			ItemSets = new ItemSetCollection(extendedGrammar);
 			OnStartItemSetsCalculation();
 			ItemSets.Initialize();
@@ -22,7 +21,7 @@ namespace Parser.LR.CLR {
 						ActionTable[state, Terminal.Terminator] = ActionFactory.AcceptAction;
 					else if (item.NextSymbol is null) {
 						var reduceAction = ActionFactory.CreateReduceAction(item.ProductionRule);
-						if (ActionTable[state, item.Lookahead] is {Type: not ActionType.Error} action) {
+						if (checkConflicts && ActionTable[state, item.Lookahead] is {Type: not ActionType.Error} action) {
 							if (action.Equals(reduceAction))
 								continue;
 							throw new ActionConflictException<Item> {State = state, Terminal = item.Lookahead, ConflictingActions = new[] {action, reduceAction}};
@@ -32,7 +31,7 @@ namespace Parser.LR.CLR {
 					else if (item.NextSymbol.IsTerminal) {
 						var nextTerminal = item.NextSymbol.AsTerminal;
 						var shiftAction = ActionFactory.CreateShiftAction(ItemSets.Go(state, nextTerminal));
-						if (ActionTable[state, nextTerminal] is {Type: not ActionType.Error} action) {
+						if (checkConflicts && ActionTable[state, nextTerminal] is {Type: not ActionType.Error} action) {
 							if (action.Equals(shiftAction))
 								continue;
 							throw new ActionConflictException<Item> {State = state, Terminal = nextTerminal, ConflictingActions = new[] {action, shiftAction}};
@@ -41,13 +40,7 @@ namespace Parser.LR.CLR {
 					}
 					else {
 						var nextSymbol = item.NextSymbol;
-						var gotoState = ItemSets.Go(state, nextSymbol);
-						if (GotoTable[state, nextSymbol.AsNonterminal] is { } existingState) {
-							if (existingState.Equals(gotoState))
-								continue;
-							throw new GotoConflictException<Item> {State = state, Nonterminal = nextSymbol.AsNonterminal, ConflictingGotoStates = new[] {existingState, gotoState}};
-						}
-						GotoTable[state, nextSymbol.AsNonterminal] = gotoState;
+						GotoTable[state, nextSymbol.AsNonterminal] = ItemSets.Go(state, nextSymbol);
 					}
 			}
 			OnCompleteTableCalculation();
