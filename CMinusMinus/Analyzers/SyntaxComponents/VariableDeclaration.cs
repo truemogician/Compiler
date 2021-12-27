9efdd4ad42872ee9f196c9362fed433f57b0f974
true_mogician <925269713@qq.com>
2021-12-27 17:48:44 +0800

    feat: VariableDeclaration

---
+++ CMinusMinus/Analyzers/SyntaxComponents/VariableDeclaration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Analyzer;
+using Parser;
+using TrueMogician.Extensions.Enumerable;
+
+namespace CMinusMinus.Analyzers.SyntaxComponents {
+	public class VariableDeclaration {
+		public VariableDeclaration(IEnumerable<SyntaxTreeNode> nodes) {
+			var nds = nodes.AsArray();
+			var i = 0;
+			for (; i < nds.Length && nds[i].Value.TerminalInstance?.Terminal.Lexeme.Name != nameof(LexemeType.Identifier); ++i) { }
+			if (i == nds.Length)
+				throw new UnexpectedSyntaxNodeException();
+			Type = new FullType(nds[..i]);
+			Name = nds[i++].Value.AsTerminalInstance.Token.Value;
+			if (i < nds.Length) {
+				ThrowHelper.IsTerminal(nds[i++], "=");
+				if (i == nds.Length)
+					throw new UnexpectedSyntaxNodeException();
+				DefaultValue = nds[i++];
+				if (i != nds.Length)
+					throw new UnexpectedSyntaxNodeException();
+			}
+		}
+
+		public FullType Type { get; }
+
+		public string Name { get; }
+
+		public Expression? DefaultValue { get; }
+
+		public static IEnumerable<VariableDeclaration> FromStatement(SyntaxTreeNode node) {
+			ThrowHelper.IsNonterminal(node, NonterminalType.DeclarationStatement);
+			throw new NotImplementedException();
+		}
+	}
+}
