22c8b02be730d8ae9bd86d13c7169184afc4b97c
true_mogician <925269713@qq.com>
2021-12-27 15:05:10 +0800

    feat: SyntaxTree.Clean
    refactor: inherit from TrueMogician.Tree<T>

--- Parser/SyntaxTree.cs
+++ Parser/SyntaxTree.cs
@@ -1,17 +1,28 @@
 ﻿using System;
+using TrueMogician.Extensions.Collections.Tree;
 
 namespace Parser {
-	public class SyntaxTree : IFormattable {
-		public SyntaxTree(SyntaxTreeNode root) => Root = root;
-
-		public SyntaxTreeNode Root { get; }
-
-		public static implicit operator SyntaxTree(SyntaxTreeNode node) => new(node);
+	public class SyntaxTree : Tree<SyntaxTreeNode>, IFormattable {
+		public SyntaxTree(SyntaxTreeNode root) : base(root) { }
 
 		public string ToString(bool skipTempNonterminal) => Root.ToString(0, skipTempNonterminal);
 
 		public override string ToString() => ToString(true);
 
 		public string ToString(string? format, IFormatProvider? formatProvider) => Root.ToString(format, formatProvider);
+
+		public void Clean() {
+			if (Root.Value.Nonterminal is { Temporary: true })
+				throw new InvalidOperationException("Root is a temporary node");
+			foreach (var node in Root)
+				if (node.Value.Nonterminal is { Temporary: true }) {
+					var parent = node.Parent!;
+					int index = parent.Children.IndexOf(node);
+					node.Parent = null;
+					parent.Children.InsertRange(index, node.Children);
+				}
+		}
+
+		public static implicit operator SyntaxTree(SyntaxTreeNode node) => new(node);
 	}
 }
