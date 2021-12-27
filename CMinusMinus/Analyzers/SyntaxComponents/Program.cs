567adbe2265de9d0c93649d63e7445fb86df04f3
true_mogician <925269713@qq.com>
2021-12-27 17:47:33 +0800

    feat: Program

---
+++ CMinusMinus/Analyzers/SyntaxComponents/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Analyzer;
+using Parser;
+
+namespace CMinusMinus.Analyzers.SyntaxComponents {
+	public class Program {
+		public Program(SyntaxTreeNode node) {
+			ThrowHelper.IsNonterminal(node, NonterminalType.Program);
+			var functions = new List<FunctionDeclaration>();
+			var globals = new List<VariableDeclaration>();
+			foreach (var child in node.Children)
+				switch (child.Value.Nonterminal?.Name) {
+					case nameof(NonterminalType.FunctionDeclaration):
+						functions.Add(child);
+						break;
+					case nameof(NonterminalType.DeclarationStatement):
+						globals.AddRange(VariableDeclaration.FromStatement(child));
+						break;
+					default: throw new UnexpectedSyntaxNodeException { Node = child };
+				}
+			FunctionDeclarations = functions;
+			GlobalVariableDeclarations = globals;
+		}
+
+		public IReadOnlyList<FunctionDeclaration> FunctionDeclarations { get; }
+
+		public IReadOnlyList<VariableDeclaration> GlobalVariableDeclarations { get; }
+
+		public static implicit operator Program(SyntaxTreeNode node) => new(node);
+	}
+}
