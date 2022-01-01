1dc099cad8a48dccc8b6a095a5533e106d621e3e
true_mogician <925269713@qq.com>
2022-01-02 00:59:46 +0800

    feat: AnalyzerCollection

---
+++ Analyzer/AnalyzerCollection.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Parser;
+using TrueMogician.Exceptions;
+using TrueMogician.Extensions.Collections.Tree;
+using TrueMogician.Extensions.Enumerable;
+using TrueMogician.Extensions.Reflection;
+
+namespace Analyzer {
+	using AnalyzerNode = ValuedTreeNode<IAnalyzer>;
+
+	public class AnalyzerCollection : ICollection<IAnalyzer> {
+		private static readonly Type _analyzerInterface = typeof(IAnalyzer<,>);
+
+		private static readonly Type _rootAnalyzerInterface = typeof(IRootAnalyzer<>);
+
+		private static readonly MethodInfo _addRootAnalyzerInfo = typeof(AnalyzerCollection).GetMethods(BindingFlags.Public | BindingFlags.Instance).Single(m => m.Name == nameof(Add) && m.ContainsGenericParameters);
+
+		private readonly List<AnalyzerNode> _rootAnalyzers = new();
+
+		public IEnumerator<IAnalyzer> GetEnumerator() => _rootAnalyzers.SelectMany(a => a.Select(n => n.Value)).GetEnumerator();
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+		public void Add(IAnalyzer item) {
+			if (item.GetType().GetGenericInterface(_rootAnalyzerInterface) is null)
+				throw new InterfaceNotImplementedException(_rootAnalyzerInterface);
+			var targetType = item.GetType().GetGenericInterfaceArguments(_rootAnalyzerInterface)!.Single();
+			_addRootAnalyzerInfo.MakeGenericMethod(targetType).Invoke(this, item);
+		}
+
+		public void Clear() => _rootAnalyzers.Clear();
+
+		public bool Contains(IAnalyzer item) => ((IEnumerable<IAnalyzer>)this).Contains(item);
+
+		public void CopyTo(IAnalyzer[] array, int arrayIndex) => this.ToArray().CopyTo(array, arrayIndex);
+
+		public bool Remove(IAnalyzer item) {
+			foreach (var root in _rootAnalyzers)
+				if (root.Value.Equals(item)) {
+					_rootAnalyzers.Remove(root);
+					return true;
+				}
+				else {
+					var target = root.Descendents.FirstOrDefault(n => n.Value.Equals(item));
+					if (target is not null) {
+						AnalyzerNode.Unlink(target);
+						return true;
+					}
+				}
+			return false;
+		}
+
+		public int Count => _rootAnalyzers.Sum(n => n.Count());
+
+		public bool IsReadOnly => false;
+
+		public AnalyzerNode Add<T>(IRootAnalyzer<T> rootAnalyzer) {
+			var node = new AnalyzerNode(rootAnalyzer);
+			_rootAnalyzers.Add(node);
+			return node;
+		}
+
+		public AnalyzerNode Add(IAnalyzer item, IAnalyzer dependency) {
+			var (type, _) = GetTypeParameters(item);
+			var (_, dst) = GetTypeParameters(dependency);
+			if (!type.IsAssignableFrom(dst))
+				throw new InvariantTypeException(type, dst);
+			AnalyzerNode? parent = null;
+			foreach (var node in _rootAnalyzers.SelectMany(n => n)) {
+				if (node.Value.Equals(item))
+					throw new InvalidOperationException($"Analyzer {item.Name} already added");
+				if (node.Value.Equals(dependency))
+					parent = node;
+			}
+			if (parent is null) {
+				if (dependency.GetType().GetGenericInterface(_rootAnalyzerInterface) is not null)
+					parent = (AnalyzerNode)_addRootAnalyzerInfo.MakeGenericMethod(dst).Invoke(this, dependency);
+				else
+					throw new InvalidOperationException($"Dependency analyzer {dependency.Name} not found");
+			}
+			return new AnalyzerNode(item, parent);
+		}
+
+		public IEnumerable<SemanticError> Analyze(SyntaxTree tree) {
+			var results = new Dictionary<IAnalyzer, object>();
+			var queue = new Queue<AnalyzerNode>();
+			foreach (var root in _rootAnalyzers) {
+				results[root.Value] = root.Value.Analyze(tree, out var errors);
+				foreach (var error in errors)
+					yield return error;
+				root.Children.Each(child => queue.Enqueue(child));
+			}
+			while (queue.Count > 0) {
+				var cur = queue.Dequeue();
+				results[cur.Value] = cur.Value.Analyze(results[cur.Parent!.Value], out var errors);
+				foreach (var error in errors)
+					yield return error;
+				cur.Children.Each(child => queue.Enqueue(child));
+			}
+		}
+
+		private static (Type Source, Type Target) GetTypeParameters(IAnalyzer analyzer) {
+			var types = analyzer.GetType().GetGenericInterfaceArguments(_analyzerInterface);
+			if (types is null)
+				throw new InterfaceNotImplementedException(_analyzerInterface);
+			return (types[0], types[1]);
+		}
+	}
+}
--- Language/Language.cs
+++ Language/Language.cs
@@ -1,23 +1,11 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Analyzer;
 using Lexer;
 using Parser;
 
-#nullable enable
 namespace Language {
 	public abstract class Language<TLexer, TParser, TFactory> : ILanguage where TLexer : ILexer where TParser : IParser where TFactory : LanguageFactoryBase, new() {
-		public static Lexicon Lexicon => Factory.Lexicon;
-
-		public static Grammar Grammar => Factory.Grammar;
-
-		public abstract TLexer Lexer { get; }
-
-		public abstract TParser Parser { get; }
-
-		public ICollection<IAnalyzer> Analyzers { get; } = new List<IAnalyzer>();
-
-		protected static TFactory Factory { get; } = new();
+		AnalyzerCollection ILanguage.Analyzers => Analyzers;
 
 		ILexer ILanguage.Lexer => Lexer;
 
@@ -32,6 +20,24 @@ namespace Language {
 		public SyntaxTree Parse(string code) => Parser.Parse(Filter(Tokenize(code)));
 
 		public bool TryParse(string code, out SyntaxTree? ast) => Parser.TryParse(Filter(Tokenize(code)), out ast);
+
+		public IEnumerable<SemanticError> Analyze(string code) {
+			var tree = Parse(code);
+			tree.Clean();
+			return Analyzers.Analyze(tree);
+		}
+
+		public static Lexicon Lexicon => Factory.Lexicon;
+
+		public static Grammar Grammar => Factory.Grammar;
+
+		public static AnalyzerCollection Analyzers => Factory.Analyzers;
+
+		public abstract TLexer Lexer { get; }
+
+		public abstract TParser Parser { get; }
+
+		protected static TFactory Factory { get; } = new();
 	}
 
 	public interface ILanguage {
@@ -39,7 +45,7 @@ namespace Language {
 
 		public IParser Parser { get; }
 
-		public ICollection<IAnalyzer> Analyzers { get; }
+		public AnalyzerCollection Analyzers { get; }
 
 		public IEnumerable<Token> Tokenize(string code) => Lexer.Tokenize(code);
 
@@ -54,7 +60,7 @@ namespace Language {
 		public IEnumerable<SemanticError> Analyze(string code) {
 			var tree = Parse(code);
 			tree.Clean();
-			return Analyzers.SelectMany(analyzer => analyzer.Analyze(tree));
+			return Analyzers.Analyze(tree);
 		}
 	}
 }
