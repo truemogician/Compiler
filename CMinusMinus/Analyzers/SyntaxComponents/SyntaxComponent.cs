2c79ad36c8d4585672f10cfcd908d31f381f0841
true_mogician <925269713@qq.com>
2022-01-08 22:56:40 +0800

    feat: properties dictionary

--- CMinusMinus/Analyzers/SyntaxComponents/SyntaxComponent.cs
+++ CMinusMinus/Analyzers/SyntaxComponents/SyntaxComponent.cs
@@ -1,14 +1,21 @@
-﻿using Analyzer;
+﻿using System.Collections.Generic;
+using Analyzer;
 using Parser;
 
 namespace CMinusMinus.Analyzers.SyntaxComponents {
 	public abstract class SyntaxComponent {
+		protected SyntaxComponent(SyntaxTreeNode node) => Node = node;
+
 		internal SyntaxTreeNode Node { get; }
 
-		protected SyntaxComponent(SyntaxTreeNode node) => Node = node;
+		internal Dictionary<string, object?> Properties { get; } = new();
 
 		internal SemanticError CreateError(SemanticErrorType type) => new(type, Node);
 
 		internal SemanticError CreateError(SemanticErrorType type, string message) => new(type, Node) { Message = message };
+
+		internal T? GetProperty<T>(string key) => (T?)Properties[key];
+
+		internal void SetProperty<T>(string key, T? value) => Properties[key] = value;
 	}
 }
