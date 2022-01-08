51e43a2641ad75dee3f34aba0ffdf73b4c3706c6
true_mogician <925269713@qq.com>
2022-01-08 21:58:23 +0800

    feat: IIntermediateCodeGenerator

---
+++ IntermediateCodeGenerator/IIntermediateCodeGenerator.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntermediateCodeGenerator {
+	public interface IIntermediateCodeGenerator<in TSource, TOperation, TOperand> where TOperation : struct, Enum where TOperand : class {
+		public IEnumerable<Quaternion<TOperation, TOperand>> Generate(TSource source);
+	}
+}
