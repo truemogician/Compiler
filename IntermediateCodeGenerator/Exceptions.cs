fea8b90a66179861b644e9d50f763033ae1c3800
true_mogician <925269713@qq.com>
2022-01-08 22:53:42 +0800

    feat: SemanticErrorException

---
+++ IntermediateCodeGenerator/Exceptions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Analyzer;
+using TrueMogician.Exceptions;
+
+namespace IntermediateCodeGenerator {
+	public class SemanticErrorException : ExceptionWithDefaultMessage {
+		public IReadOnlyList<SemanticError>? Errors { get; init; }
+
+		protected override string? DefaultMessage => "Unhandled semantic errors detected";
+	}
+}
