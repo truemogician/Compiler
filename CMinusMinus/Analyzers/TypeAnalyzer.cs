92d0f35133ad52d406d8f4634d8e9a4f2a58733d
true_mogician <925269713@qq.com>
2022-01-12 15:26:29 +0800

    feat: TypeAnalyzer skeleton

---
+++ CMinusMinus/Analyzers/TypeAnalyzer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Analyzer;
+using CMinusMinus.Analyzers.SyntaxComponents;
+
+namespace CMinusMinus.Analyzers {
+	public class TypeAnalyzer : IReadOnlyAnalyzer<Program> {
+		private static readonly SemanticErrorType TypeNotMatchedError = new("TP0001", ErrorLevel.Error, Name) { DefaultMessage = "Identifier not defined." };
+
+		private static readonly SemanticErrorType InvalidCastError = new("TP0002", ErrorLevel.Error, Name) { DefaultMessage = "Identifier already declared." };
+
+		private static readonly SemanticErrorType PrecisionLostWarning = new("TP0003", ErrorLevel.Warning, Name) { DefaultMessage = "Identifier is never used." };
+
+		string IAnalyzer.Name => nameof(TypeAnalyzer);
+
+		IEnumerable<SemanticError> IReadOnlyAnalyzer<Program>.Analyze(Program source) {
+			yield break;
+		}
+
+		public static string Name => nameof(TypeAnalyzer);
+	}
+}
